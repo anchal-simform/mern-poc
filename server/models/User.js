c329667375115ef038b8520671c124edab60b9f5
MiSpu7R#ce <meet.k.patel@simformsolutions.com>
2023-10-18 12:48:42 +0530

    Initial backend setup with graphql with user authentication added

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
